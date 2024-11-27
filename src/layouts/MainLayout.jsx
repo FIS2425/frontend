87df5787e64ab3d20ba2c378b649e095f6035dc5
antonio <antoniorodriguezruiz2002@gmail.com>
2024-11-27 21:42:11 +0100

    feat: initialized application header

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,20 +1,13 @@
-import { Link, Outlet } from 'react-router';
+import { Outlet } from 'react-router';
+import { Header } from '@/components/layouts/header';
 
 export default function MainLayout() {
   return (
-    <main className="flex-1 mt-[64px]">
-      <nav className="flex justify-between fixed top-0 left-0 w-full bg-secondary shadow-md p-5 z-10">
-        <div />
-        <div className="flex align-items-center gap-x-4">
-          <Link to="/" className="text-secondary-foreground hover:underline hover:text-secondary-foreground">
-            Home
-          </Link>
-          <Link to="/about" className="text-secondary-foreground hover:underline hover:text-secondary-foreground">
-            About
-          </Link>
-        </div>
-      </nav>
-      <Outlet />
-    </main>
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-grow flex items-center justify-center">
+        <Outlet />
+      </main>
+    </div>
   );
 }
