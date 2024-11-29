ee0f073b939f896fa170dd84971b7fe5bcea9e38
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-11-29 20:05:53 +0100

    style: fixed padding problems on root

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,7 +3,7 @@ import { Header } from '@/components/layouts/header';
 
 export default function MainLayout() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="py-2 min-h-screen flex flex-col">
       <Header />
       <main className="flex-grow flex items-center justify-center">
         <Outlet />
