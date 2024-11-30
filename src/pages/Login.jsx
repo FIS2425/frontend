97f047fb1cc9dcf5e6dc6e9fe7d8e2d8396a1942
AntonioRodriguezRuiz <antoniorodriguezruiz2002@gmail.com>
2024-11-30 17:04:05 +0100

    feat: added login functionality

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,12 @@
 import { Bandage, Activity, Cross, HeartPulse, HandHeart, CalendarHeart, Syringe, Tablets, Stethoscope, Dna, Brain } from 'lucide-react';
-import { LoginForm } from '@/forms/auth';
+import { LoginForm, loginSchema } from '@/forms/auth';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import PulsingIcons from '@/components/pulsing-icons';
+import { login } from '@/services/auth';
+import { useState } from 'react';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router';
 
 export function Login() {
   const icons = {
@@ -34,6 +39,43 @@ export function Login() {
 }
 
 function LoginCard() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const form = useForm({
+    resolver: zodResolver(loginSchema),
+    defaultValues: {
+      email: '',
+      password: '',
+    },
+  });
+
+  function onSubmit(values) {
+    setIsLoading(true);
+    login(values)
+      .then((response) => {
+        if (response.status === 200 && response.data.message === 'Login successful')
+          navigate('/app');
+
+        else if (response.status === 200 && response.data.message === 'Credentials validated, please verify 2FA token')
+          navigate('/verify-2fa');
+      })
+      .catch((err) => {
+        if (err.response && err.response.status === 400) {
+          // Usually we would use form.setError, but in this case the 400 error only happens for "invalid credetials" which has no target field
+          setError('Invalid email or password');
+        } else {
+          setError('An error occurred. Please try again later.');
+          console.error(err);
+        }
+        setIconsRed();
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  }
+
   return (
     <Card className="w-full max-w-md rounded-lg shadow-sm">
       <CardHeader className="items-start">
@@ -41,8 +83,15 @@ function LoginCard() {
         <CardDescription>Log In to CloudMedix</CardDescription>
       </CardHeader>
       <CardContent>
-        <LoginForm />
+        <LoginForm form={form} onSubmit={onSubmit} isLoading={isLoading} error={error} />
       </CardContent>
     </Card>
   );
+}
+
+function setIconsRed() {
+  document.querySelectorAll('.pulse').forEach((div) => {
+    div.classList.add('error-pulse');
+    div.classList.remove('pulse');
+  });
 }
