fae86235422101bd07fdd2156a36d76ecabe4124
Alvaro Gonzalez <alvgonfri@alum.us.es>
2024-12-09 01:52:05 +0100

    feat: added 2fa verification

--- src/forms/auth/forms.jsx
+++ src/forms/auth/forms.jsx
@@ -9,6 +9,7 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { InputOTP, InputOTPGroup, InputOTPSlot } from '@/components/ui/input-otp';
 import { PasswordInput } from '@/components/forms/password-input';
 
 export function LoginForm({ form, onSubmit, isLoading, error }) {
@@ -49,6 +50,45 @@ export function LoginForm({ form, onSubmit, isLoading, error }) {
             <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
           )}
           <span className="text-base font-large">Log In</span>
+        </Button>
+      </form>
+    </Form>
+  );
+}
+
+export function Verify2FAForm({ form, onSubmit, isLoading, error }) {
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {error && <FormMessage className="flex flex-col items-start text-base">{error}</FormMessage>}
+        <FormField
+          control={form.control}
+          name="totpToken"
+          rules={{ required: true }}
+          render={({ field }) => (
+            <FormItem className="flex flex-col items-center">
+              <FormLabel className="text-base font-large">2FA Token</FormLabel>
+              <FormControl>
+                <InputOTP maxLength={6} {...field}>
+                  <InputOTPGroup>
+                    <InputOTPSlot index={0} />
+                    <InputOTPSlot index={1} />
+                    <InputOTPSlot index={2} />
+                    <InputOTPSlot index={3} />
+                    <InputOTPSlot index={4} />
+                    <InputOTPSlot index={5} />
+                  </InputOTPGroup>
+                </InputOTP>
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button className="w-full" type="submit" disabled={isLoading}>
+          {isLoading && (
+            <LoaderCircle className="mr-2 h-4 w-4 animate-spin" />
+          )}
+          <span className="text-base font-large">Verify</span>
         </Button>
       </form>
     </Form>
