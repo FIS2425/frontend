fc5b6a5a8fc932cadd0de18f3930c4b7bd1294a4
carloosbaquero <carlos.baqueville@gmail.com>
2024-12-29 12:32:48 +0100

    fix(staff): clinicId taken off from schema in services

--- src/services/staff.js
+++ src/services/staff.js
@@ -1,8 +1,8 @@
 import { client } from '@/api/axiosClient';
 
-export function registerStaff({ name, surname, email, password, specialty, clinicId, dni }) {
+export function registerStaff({ name, surname, email, password, specialty, dni }) {
   return client.post(
     '/staff/register',
-    { name, surname, email, password, specialty, clinicId, dni }
+    { name, surname, email, password, specialty, dni }
   );
 }
