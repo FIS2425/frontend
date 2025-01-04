0d062f59b2fb9f2d269dce9e2d2ceae2b963620a
Aitor Rodríguez Dueñas <72505728+AitorRD@users.noreply.github.com>
2025-01-04 16:25:56 +0100

    feat: substitution of suscribe buttons and addons (#61)

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -3,21 +3,10 @@ import { Button } from '@/components/ui/button';
 import { useIsMobile } from '@/hooks/use-mobile';
 import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from '@/components/ui/card';
 import logo from '@/assets/cloudmedix.png';
-import { useState } from 'react';
 import { Calendar, Users, FileText, Shield } from 'lucide-react';
 
 export function Landing() {
-  const [setSelectedPlan] = useState(null);
-  const [selectedAddOn, setSelectedAddOn] = useState(null);
   const isMobile = useIsMobile();
-
-  const handleSelectPlan = (plan) => {
-    setSelectedPlan(plan);
-  };
-
-  const handleSelectAddOn = (addonName) => {
-    setSelectedAddOn((prevSelectedAddOn) => (prevSelectedAddOn === addonName ? null : addonName));
-  };
 
   const plans = [
     {
@@ -162,13 +151,6 @@ export function Landing() {
                   <h3 className="text-xl font-bold mb-2">Add-Ons</h3>
                   {plan.addons.map((addon) => (
                     <div key={addon.name} className="mb-4">
-                      <input
-                        type="radio"
-                        name="addon"
-                        checked={selectedAddOn === addon.name}
-                        onClick={() => handleSelectAddOn(addon.name)}
-                        className="form-radio h-4 w-4 text-blue-600 mr-2"
-                      />
                       <span className="font-semibold">{addon.name}</span>
                       <p>{addon.description}</p>
                       <p>Precio: {addon.price}</p>
@@ -178,9 +160,11 @@ export function Landing() {
               )}
             </CardContent>
             <CardFooter className="flex justify-center items-center mt-auto">
-              <Button className="bg-blue-500 text-white px-4 py-2 rounded-lg" onClick={() => handleSelectPlan(plan.plan)}>
-                Subscribe
-              </Button>
+              <a href="https://github.com/orgs/FIS2425/people" target="_blank">
+                <Button className="bg-blue-500 text-white px-4 py-2 rounded-lg">
+                  Contact Us
+                </Button>
+              </a>
             </CardFooter>
           </Card>
         ))}
