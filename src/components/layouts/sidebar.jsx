40d89ee8613dd0a5fbab2556cf18aa5afd9d397b
carloosbaquero <carlos.baqueville@gmail.com>
2025-01-07 21:16:28 +0100

    feat: icons clinic updated

--- src/components/layouts/sidebar.jsx
+++ src/components/layouts/sidebar.jsx
@@ -1,4 +1,4 @@
-import { Calendar, Home, Search, ChevronRight, ChevronsUpDown, LogOut, CalendarCheck, UserPen, ClipboardPlus, Stethoscope } from 'lucide-react';
+import { Calendar, Home, Search, ChevronRight, ChevronsUpDown, LogOut, CalendarCheck, UserPen, ClipboardPlus, Stethoscope, Hospital } from 'lucide-react';
 import { Link } from 'react-router';
 import { MoreHorizontal } from 'lucide-react';
 import {
@@ -76,7 +76,7 @@ const data = [
       {
         title: 'Clinic actions',
         url: '#',
-        icon: UserPen,
+        icon: Hospital,
         roles: ['admin','clinicadmin'],
         items: [
           {
